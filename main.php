0b558b4608ab9b3e3785102275a3389cc9eb106d
Pranav <pranav@nadig.in>
2019-04-17 12:39:09 -0400

    Updated SQL credentials

--- main.php
+++ main.php
@@ -16,14 +16,14 @@
     crossorigin=""></script>
 
     <style>
-            #mapid { height: 600px; 
+            #mapid { height: 600px;
             z-index: 1;
             }
-            
-            
+
+
             body {font-family: Arial, Helvetica, sans-serif;}
             * {box-sizing: border-box;}
-            
+
             /* Button used to open the contact form - fixed at the bottom of the page */
             .open-button {
               background-color: #555;
@@ -37,7 +37,7 @@
               right: 28px;
               width: 280px;
             }
-            
+
             /* The popup form - hidden by default */
             .form-popup {
               display: none;
@@ -47,7 +47,7 @@
               border: 3px solid #f1f1f1;
               z-index: 1000;
             }
-            
+
             /* Add styles to the form container */
             .form-container {
               max-width: 300px;
@@ -56,7 +56,7 @@
 /*              z-index: 1;*/
 
             }
-            
+
             /* Full-width input fields */
             .form-container input[type=text], .form-container input[type=password] {
               width: 100%;
@@ -65,13 +65,13 @@
               border: none;
               background: #f1f1f1;
             }
-            
+
             /* When the inputs get focus, do something */
             .form-container input[type=text]:focus, .form-container input[type=password]:focus {
               background-color: #ddd;
               outline: none;
             }
-            
+
             /* Set a style for the submit/login button */
             .form-container .btn {
               background-color: #4CAF50;
@@ -83,17 +83,17 @@
               margin-bottom:10px;
               opacity: 0.8;
             }
-            
+
             /* Add a red background color to the cancel button */
             .form-container .cancel {
               background-color: red;
             }
-            
+
             /* Add some hover effects to buttons */
             .form-container .btn:hover, .open-button:hover {
               opacity: 1;
             }
-            
+
             </style>
 </head>
 <body>
@@ -101,7 +101,7 @@
     <div>
             <button onclick="mainApp.logOut()">LogOut</button>
 
-           
+
 
             <form action="action_page.php" method = "POST" class="form-container">
                 <h1>Login</h1>
@@ -126,12 +126,12 @@
                 </select>
                 <label for="description"><b>Description</b></label>
                 <input type="text" placeholder="Description:" name="description" required>
-                
+
                 <label for="location"><b>Location</b></label>
                 <input type="text" placeholder="Location:" name="location" required id="lcn">
-                
+
                 <input type="submit" value="submit" name="submit">
-                
+
 <!--                <button type="button" class="btn cancel" onclick="closeForm()">Close</button>-->
               </form>
 
@@ -142,9 +142,9 @@
 
 
 <div class="form-popup" id="myForm">
-  
+
 </div>
-    
+
     <script src="firebase.js"></script>
     <script src="main.js"></script>
     <script>
@@ -156,26 +156,26 @@
             id: 'mapbox.streets',
             accessToken: 'pk.eyJ1IjoiZ3RtcHJrc2hyYiIsImEiOiJjamZ0bXBqZnMxd3E5MnduejVjdGpuN2R4In0.vvrRpEdZWNwaKUO6vmgRHw'
         }).addTo(mymap);
-        
-        
+
+
 		mymap.locate({setView: true, maxZoom: 16});
 		function onLocationFound(e) {
 		var radius = e.accuracy / 2;
 
 		L.marker(e.latlng).addTo(mymap)
 			//.bindPopup("You are within " + radius + " meters from this point").openPopup();
-            
+
 
 		L.circle(e.latlng, radius).addTo(mymap);
 //            document.getElementById('lat').innerHTML = e.latlng;
 
 		}
-				
+
 		function onLocationError(e) {
 		alert(e.message);
 		}
-		mymap.on('locationerror', onLocationError); 
-        
+		mymap.on('locationerror', onLocationError);
+
         mymap.on('locationfound', onLocationFound);
 
         function onLocationFound(e) {
@@ -188,7 +188,7 @@
             a
         }
         mymap.on('locationfound', onLocationFound);
-        
+
 
         function onMapClick(e) {
             var location = e.latlng;
@@ -196,18 +196,18 @@
             var one = Math.round(e.latlng.lat * 100000)/100000;
             var two = Math.round(e.latlng.lng * 100000)/100000;
             var res = one + "," + two;
-        
+
           //  alert(one);
-            
+
           //  loc.push(location)
-            
+
             alert("You clicked the map at " + location);
             document.getElementById('locationdisplay').innerHTML = location;
             document.getElementById('lat').value = one;
             document.getElementById('long').value = two;
-            
+
             document.getElementById('lcn').value= res;
-          //  alert(res[0]);        
+          //  alert(res[0]);
         }
 
         mymap.on('click', onMapClick);
@@ -226,7 +226,7 @@
 <?php
 $servername = "localhost";
 $username = "root";
-$password = "";
+$password = "r3apb3n3fit";
 $dbname = "nd_manager";
 
 // Create connection
@@ -234,7 +234,7 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
-} 
+}
 
 $sql = "SELECT latitude, longitude from data";
 $result = $conn->query($sql);
@@ -246,9 +246,9 @@ if ($result->num_rows > 0) {
         echo("<script> L.marker([$floatlat, $floatlng]).addTo(mymap);
         </script>");
  //    echo("<script> console.log($floatlat); </script>");
-        
+
  }
-    
+
 } else {
     echo "0 results";
 }
