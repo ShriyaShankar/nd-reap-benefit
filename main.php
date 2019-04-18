a2124b1f79b99a4606a060f71fed07d6c13b81fd
Pranav <pranav@nadig.in>
2019-04-18 09:30:44 -0400

    Location debug 2

--- main.php
+++ main.php
@@ -167,7 +167,8 @@
 		function onLocationFound(e) {
 		var radius = e.accuracy / 2;
 
-		L.marker(e.latlng).addTo(mymap).bindPopup("You are within " + radius + " meters from this point").openPopup();
+		L.marker(e.latlng).addTo(mymap)
+			//.bindPopup("You are within " + radius + " meters from this point").openPopup();
 
 
 		L.circle(e.latlng, radius).addTo(mymap);
@@ -185,7 +186,8 @@
         function onLocationFound(e) {
             var radius = e.accuracy / 2;
 
-            L.marker(e.latlng).addTo(mymap);//.bindPopup("You are within " + radius + " meters from this point").openPopup();
+            L.marker(e.latlng).addTo(mymap)
+              //  .bindPopup("You are within " + radius + " meters from this point").openPopup();
 
             L.circle(e.latlng, radius).addTo(mymap);
         }
