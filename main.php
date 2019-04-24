a2b5ceee69e53876aae9cd8e7a9d24ae830e68f5
Pranav <pranav@nadig.in>
2019-04-24 00:40:10 -0400

    Enabled location popup

--- main.php
+++ main.php
@@ -185,8 +185,7 @@
         function onLocationFound(e) {
             var radius = e.accuracy / 2;
 
-            L.marker(e.latlng).addTo(mymap)
-              //  .bindPopup("You are within " + radius + " meters from this point").openPopup();
+            L.marker(e.latlng).addTo(mymap).bindPopup("You are within " + radius + " meters from this point").openPopup();
 
             L.circle(e.latlng, radius).addTo(mymap);
         }
