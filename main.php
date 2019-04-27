959e32386b5c120b448e837d0ec2067a66759e6a
Gautam Prakash <gautamp@reapbenefit.org>
2019-04-27 17:17:02 +0530

    Updated error on line 226, maon.php

--- main.php
+++ main.php
@@ -223,7 +223,7 @@ $password = "S0lvesm@lld3ntbig";
 $dbname = "nd_manager";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password);
+$conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
