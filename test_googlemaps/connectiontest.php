115cde27b9e36b61074864dea08c952de4e546e7
Gautam Prakash <gautamp@reapbenefit.org>
2019-04-27 18:23:01 +0530

    Updated connection to PDO model

--- test_googlemaps/connectiontest.php
+++ test_googlemaps/connectiontest.php
@@ -4,12 +4,21 @@ $username = "root";
 $password = "S0lvesm@lld3ntbig";
 $dbname = "nd_manager";
 $table = "data";
-// Create connection
-$conn = new mysqli($servername, $username, $password);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
-echo "Connected successfully";
-$conn->close();
+// // Create connection
+// $conn = new mysqli($servername, $username, $password);
+// // Check connection
+// if ($conn->connect_error) {
+//     die("Connection failed: " . $conn->connect_error);
+// } 
+// echo "Connected successfully";
+// $conn->close();
+
+$dbh = new PDO('mysql:host=srv-captain--mysqldb-db;dbname=nd_manager', $username, $password);
+if ($dbh != null) {
+    echo "Connected";
+} catch (PDOException $e) {
+    print "Error!: " . $e->getMessage() . "<br/>";
+    die();
+}
 ?>
+
