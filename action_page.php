d1c3985b94a92404d2eacff8f6f8a709828c887e
Shriya Shankar <shriya.shankar19@gmail.com>
2019-04-28 11:56:49 +0530

    test

--- action_page.php
+++ action_page.php
@@ -33,8 +33,8 @@ if(isset($_POST['submit'])){
     $description=$_POST['description'];
     $category=$_POST['category'];
     $location=$_POST['location'];
-    $sql = "INSERT INTO data (name, category, description, location)
-    VALUES ('$name', '$category', '$description', '$location')";
+    $sql = "INSERT INTO data (name, latitude, longitude, category, description, location)
+    VALUES ('$name', '$latitude', '$longitude', '$category', '$description', '$location')";
 
 if ($conn->query($sql) === TRUE) {
     echo "Redirecting... ";
