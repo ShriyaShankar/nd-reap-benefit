f2c2f24258bc6a448dbb1e8e2b03500049c98bf1
Shriya Shankar <shriya.shankar19@gmail.com>
2019-04-28 12:28:00 +0530

    test

--- main.php
+++ main.php
@@ -276,6 +276,6 @@ if ($result->num_rows > 0) {
 } else {
     echo "0 results";
 }
-echo("<script> alert("Record added!"); </script>");
+echo("<script> alert(\"Record added!\"); </script>");
 $conn->close();
 ?>
