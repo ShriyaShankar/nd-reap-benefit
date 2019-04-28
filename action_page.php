68cd77417cb314bbbf2633c5f1130d02a6c5b759
Shriya Shankar <shriya.shankar19@gmail.com>
2019-04-28 12:00:27 +0530

    test

--- action_page.php
+++ action_page.php
@@ -28,8 +28,8 @@ if(isset($_POST['submit'])){
     echo "Record submitted. ";
 
     $name=$_POST['name'];
-//    $latitude=$_POST['latitude'];
-//    $longitude=$_POST['longitude'];
+    $latitude=$_POST['latitude'];
+    $longitude=$_POST['longitude'];
     $description=$_POST['description'];
     $category=$_POST['category'];
     $location=$_POST['location'];
