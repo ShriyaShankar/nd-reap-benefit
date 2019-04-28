d3fb83560b55fcc51fc117c90ca30515be6e123e
Pranav <pranav@nadig.in>
2019-04-27 22:52:23 -0400

    Revert "Redirecting all firebase users from login page"
    This reverts commit 7e13af9fe8293e6269d66f17804d91adf274e6c7.

--- login.js
+++ login.js
@@ -37,9 +37,4 @@ var ui = new firebaseui.auth.AuthUI(firebase.auth());
       // The start method will wait until the DOM is loaded.
     ui.start('#firebaseui-auth-container', uiConfig);
 
-    firebase.auth().onAuthStateChanged(function(user) {
-        if (user) {
-          window.location = '/main.php'
-        }
-
 })()
