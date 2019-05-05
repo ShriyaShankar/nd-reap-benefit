9476fa3d0d21d07947937971a39a4ba14de30a77
Shriya Shankar <shriya.shankar19@gmail.com>
2019-05-05 19:55:28 +0530

    test

--- main.php
+++ main.php
@@ -109,6 +109,10 @@
 </head>
 
 <head>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script> 
+    
 <style>
 h2 {
   position: absolute;
