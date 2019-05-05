d6f19c06fce25b5b05c39aa07984f1c86da1fbe9
Shriya Shankar <shriya.shankar19@gmail.com>
2019-05-05 22:05:13 +0530

    test

--- main.php
+++ main.php
@@ -162,11 +162,15 @@ h3 {
           <h4 class="modal-title">Learn more about the categories</h4>
         </div>
         <div class="modal-body">            
-          <p><img style = "padding: 0 5px; float: left; position:relative; LEFT:0px; TOP:0px;" src = "https://image.flaticon.com/icons/svg/1/1570.svg" width = "50" height = "50" ></p>
+          <p>
+              <img style = "padding: 0 5px; float: left; position:relative; LEFT:0px; TOP:0px;" src = "https://image.flaticon.com/icons/svg/1/1570.svg" width = "50" height = "50" >
+              <img style = "padding: 0 5px; float: left; position:relative; LEFT:0px; TOP:60px;" src = "https://image.flaticon.com/icons/svg/67/67780.svg" width = "50" height = "50" >
+            
+            </p>
           <p style = "margin-top: -10px";></p>
           <p>Let us try to say goodbye to Garbage City and help Bengaluru go back to being Garden City by reporting garbage accumulation and dump sites in our  neighbourhood </p>
            
-          <p><img style = "padding: 0 5px; float: left; position:relative; LEFT:0px; TOP:0px;" src = "https://image.flaticon.com/icons/svg/67/67780.svg" width = "50" height = "50" ></p>
+          <p></p>
           <p style = "margin-top: -10px";></p>
           <p> Water </p>
             
