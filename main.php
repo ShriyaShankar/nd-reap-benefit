80d075268b6efb74f8a52aa2e17cc94ea83008ca
Shriya Shankar <shriya.shankar19@gmail.com>
2019-05-06 22:32:59 +0530

    test

--- main.php
+++ main.php
@@ -161,7 +161,7 @@ h3 {
   <div class="modal-dialog modal-dialog-scrollable" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalScrollableTitle">Modal title</h5>
+        <h5 class="modal-title" id="exampleModalScrollableTitle">Learn more about the categories</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
@@ -180,7 +180,6 @@ h3 {
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
       </div>
     </div>
   </div>
