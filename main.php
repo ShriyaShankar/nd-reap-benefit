eb28fc414896d633ec529289eb92caefbc442b21
Shriya Shankar <shriya.shankar19@gmail.com>
2019-05-06 23:29:35 +0530

    test

--- main.php
+++ main.php
@@ -252,8 +252,8 @@ input.radio {
             <!--   <div class="form-group">
                 Severity: <br> <p class="text-left" style="display:inline !important;"> Low </p> <p class="text-right" style="display:inline !important; float:right !important;">High</p><input type="range" name="severity" min="0" max="5" />
               </div> -->
+                <label><br><b>Status of Problem: <br><b/></label>
                <fieldset>
-                   <label><br><b>Status of Problem: <br><b/></label>
                   <div class="some-class">
                     <input type="radio" class="radio" name="x" value="y" id="y" />
                     <label for="y">Mild</label>
