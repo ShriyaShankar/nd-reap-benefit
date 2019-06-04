483f79ef94940ff0964285ecaa3952507801d247
Pranav Shikarpur <me@snpranav.com>
2019-06-04 00:29:29 -0700

    Updated php to work with new db structure

--- action_page.php
+++ action_page.php
@@ -43,11 +43,11 @@
         $FloodImageURL = $_POST['floodImageURL'];
         
         //Get row where category is equal to the subcategory
-        $getcategory = "Select * from enumerated_category where $category=sub_category"
+        $getcategory = "Select * from enumerated_category where $category=sub_category";
         $getcategoryresult = $conn->query($getcategory);
         $getcategoryrow = $result->fetch_assoc();
         $category = $getcategoryrow[id];    //Assigning the category to the primary key of the enumerated_category table
-        echo("<script>console.log($category)</script>")
+        echo("<script>console.log($category)</script>");
 
 
         // Query to insert values into database
