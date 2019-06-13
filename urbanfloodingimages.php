72f1d0528671695e05ddd232818ea5aca6621864
Pranav Shikarpur <me@snpranav.com>
2019-06-12 23:48:03 -0700

    Addeds urban flooding image page

--- urbanfloodingimages.php
+++ urbanfloodingimages.php
@@ -6,13 +6,48 @@
     <title>ND Manager</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="http://2019.kmun.in/css/agency.min.css">
     </head>
     <body>
-        <div class="row" id="floodimage">
+        <!-- Portfolio Grid -->
+    <section id="portfolio">
+      <div class="container">
+        <div class="row">
+          <!-- <div class="col-lg-12 text-center"> -->
+            <h2 class="section-heading text-uppercase">Images</h2>
+            <h3 class="section-subheading"></h3>
+</div>
+<div class="row" id="images">
+</div>
+         <!-- </div> -->
         </div>
+</div>
+</section>
 
         <script>
             var imgArray = [];
+        </script>
+
+        <script>
+                for(int i=0; i<imgArray.length; i++)
+                {
+                    document.getElementById(images).appendChild(`
+                    <div class="col-md-4 col-sm-6 portfolio-item">
+            <a class="portfolio-link" data-toggle="modal" href="#portfolioModal1">
+              <div class="portfolio-hover">
+                <div class="portfolio-hover-content">
+                  <i class="fas fa-plus fa-3x"></i>
+                </div>
+              </div>
+              <img class="img-fluid" src="${imageArray[i]}" alt="" style="width:300px;height:300px; position:relative; right:-50px;">
+            </a>
+            <div class="portfolio-caption">
+              <h4>African Union</h4>
+              <p >Non Crisis</p>
+            </div>
+          </div>
+                    `)
+                }
         </script>
 
 <?php
