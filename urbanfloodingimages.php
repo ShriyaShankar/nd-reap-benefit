398f5df4f8fb1ba4bc016d6e58998fd20626cf7c
Pranav Shikarpur <me@snpranav.com>
2019-06-12 23:17:38 -0700

    Addeds urban flooding image page

--- urbanfloodingimages.php
+++ urbanfloodingimages.php
@@ -37,7 +37,7 @@
     {
         while($row = $result->fetch_assoc()) 
         {
-            $imageURL = $row["FloodImageURL"];
+            //$imageURL = $row["FloodImageURL"];
             //echo("<script>docuemnt.getElementById('floodimage').innerHTML = '<img src=\"$imageURL\" class=\"img-thumbnail\">'</script>");
         }
     }
